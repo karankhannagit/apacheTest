29c6d1f07cda2c62b48add53a588d84b6d2f8807
karankhannagit <37874511+karankhannagit@users.noreply.github.com>
2019-04-10 17:48:33 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,22 +1,18 @@
 <?php
-	
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-        'git status',
-        // 'git reset --hard HEAD',
-		// 'git submodule sync',
-		// 'git submodule update',
-		// 'git submodule status',
-	);
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output;
-	}
-	
+$build_number=$argv[1];
+$prev_build = $build_number-1;
+$gco_output = exec(       "rm -rf apacheTest" . "\n"
+                        . "git clone https://github.com/karankhannagit/apacheTest.git" . "\n"
+                        . "git -C ./apacheTest/ checkout master". "\n"
+                        . "ls -al" . "\n"
+                        . "rsync -auxz  ./apacheTest/ /efs/web/web".$build_number." --delete" . "\n"
+                        //. "rm /efs/web/html" . "\n"
+                        . "ln -s /efs/web/web".$build_number." /efs/web/html". "\n"
+                    );
+    $array = explode("\n", $gco_output);
+    $array = array_filter($array);
+    $local_commit = end($array);
+    echo ("Local Commit: " . $local_commit . "\n\n"); //last commit or local commit ?
+
 ?>
 
