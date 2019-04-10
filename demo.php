fa88ee15edd9b4dee77435d5d3ef0af6fbdc459f
karankhannagit <37874511+karankhannagit@users.noreply.github.com>
2019-04-11 00:51:18 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,18 +1,26 @@
 <?php
+$filename = './apacheTest';
 $build_number=$argv[1];
-$prev_build = $build_number-1;
-$gco_output = exec(       "rm -rf apacheTest" . "\n"
-                        . "git clone https://github.com/karankhannagit/apacheTest.git" . "\n"
-                        . "git -C ./apacheTest/ checkout master". "\n"
-                        . "ls -al" . "\n"
-                        . "rsync -auxz  ./apacheTest/ /efs/web/web".$build_number." --delete" . "\n"
-                        //. "rm /efs/web/html" . "\n"
-                        . "ln -sf /efs/web/web".$build_number." /efs/web/html". "\n"
-                    );
-    $array = explode("\n", $gco_output);
-    $array = array_filter($array);
-    $local_commit = end($array);
-    echo ("Local Commit: " . $local_commit . "\n\n"); //last commit or local commit ?
+if (file_exists($filename)) {
 
+    $gco_output = exec(
+            "cd ".$filename . "\n"
+        .   "git checkout origin/master" . "\n"
+        .   "rsync -auxz  . /efs/web/web".$build_number." --delete" . "\n"
+        .   "ln -sfn /efs/web/web".$build_number." /efs/web/html". "\n"   
+
+                                 );
+} 
+
+else {
+    echo "cloning repo";
+    $gco_output = exec(
+            "git clone https://github.com/karankhannagit/apacheTest.git" . "\n"
+        .   "rsync -auxz  ./apacheTest/ /efs/web/web".$build_number." --delete" . "\n"
+        .   "rm /efs/web/html" . "\n"
+        .   "ln -sfn /efs/web/web".$build_number." /efs/web/html". "\n"
+
+                                 );
+
+}
 ?>
-
